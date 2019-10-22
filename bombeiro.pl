6091484ff89bf58ada26d0557a09508066239778
amanda <amandamanso7@gmail.com>
2019-10-22 16:19:16 -0300

    Adicionado o 'caminho' no modelo adaptado de bloquinhos

--- bombeiro.pl
+++ bombeiro.pl
@@ -11,14 +11,24 @@ objeto(X,Y) :- not(tipo(bloquinho(X,Y,vazio))).
 
 bloquinho_livre(X, Y) :- not(objeto(X,Y)).
 
-andar_esquerda(bloquinho(X1, Y, tipo1), bloquinho(X2, Y, tipo2)) :- X2 is (X1-1), X2 > 0, not(parede(X2,Y)), not(pedras(X2,Y))
-andar_direita(bloquinho(X1, Y, tipo1), bloquinho(X2, Y, tipo2)) :- X2 is (X1+1), X2 < 11, not(parede(X2,Y)), not(pedras(X2,Y))
+%Andar para a esquerda
+caminho(bloquinho(X1, Y, tipo1), bloquinho(X2, Y, tipo2), Caminho) :- X2 is (X1-1), X2 > 0, not(parede(X2,Y)), not(pedras(X2,Y))
 
-pular_esquerda(bloquinho(X1, Y, tipo1), bloquinho(X3, Y, tipo2)) :- X2 is (X1-1), X3 is (X1-2), X3>0, pedras(X2,Y), bloquinho_livre(X3,Y)
-pular_direita(bloquinho(X1, Y, tipo1), bloquinho(X3, Y, tipo2)) :- X2 is (X1+1), X3 is (X1+2), X3<11, pedras(X2,Y), bloquinho_livre(X3,Y)
+%Andar para a direita
+caminho(bloquinho(X1, Y, tipo1), bloquinho(X2, Y, tipo2), Caminho) :- X2 is (X1+1), X2 < 11, not(parede(X2,Y)), not(pedras(X2,Y))
 
-sobe_escada(bloquinho(X, Y1, tipo1), bloquinho(X, Y2, tipo2)) :- escada(X,Y1), Y2 is (Y1+1), Y2<6, 
-desce_escada(bloquinho(X, Y1, tipo1), bloquinho(X, Y2, tipo2)) :- escada(X,Y2), Y2 is (Y1-1), Y2>0
+%Pular as pedras para a esquerda
+caminho(bloquinho(X1, Y, tipo1), bloquinho(X3, Y, tipo2), Caminho) :- X2 is (X1-1), X3 is (X1-2), X3>0, pedras(X2,Y), bloquinho_livre(X3,Y)
+
+%Pular as pedras para a direita
+caminho(bloquinho(X1, Y, tipo1), bloquinho(X3, Y, tipo2), Caminho) :- X2 is (X1+1), X3 is (X1+2), X3<11, pedras(X2,Y), bloquinho_livre(X3,Y)
+
+%Subir as escadas
+caminho(bloquinho(X, Y1, tipo1), bloquinho(X, Y2, tipo2), Caminho) :- escada(X,Y1), Y2 is (Y1+1), Y2<6, 
+
+%Descer as escadas
+caminho(bloquinho(X, Y1, tipo1), bloquinho(X, Y2, tipo2), Caminho) :- escada(X,Y2), Y2 is (Y1-1), Y2>0
+
 
 apagar(bloquinho(X, Y, item)) :- se o bombeiro está com extintor >=1 restante
 pegar_extintor(bloquinho(X, Y, item)) :- se ainda não está com o extintor na mão
@@ -28,6 +38,8 @@ objeto(X, Y) :- escada(X, Y).
 objeto(X, Y) :- foguinho(X, Y).
 objeto(X, Y) :- parede(X, Y).
 objeto(X, Y) :- pedras(X, Y).
+
+
 
 
 
